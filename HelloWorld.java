64b227d5f913cb81ee83ea3a96cca9fb6ecb444b
Chandu-0000 <144006109+Chandu-0000@users.noreply.github.com>
2025-01-09 11:57:09 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+// This is a simple Java program to print "Hello, World!"
+public class HelloWorld {
+
+    // Main method - entry point of the program
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
